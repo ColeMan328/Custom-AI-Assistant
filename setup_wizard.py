fafacec194e58e7f18f36e2940822aaefdd13a79
Cole Stoumbaugh <ctstoumbaugh@gmail.com>
2024-06-26 19:59:06 -0600

    Update setup_wizard.py
    Updated to create new readme file and fix requirements

--- setup_wizard.py
+++ setup_wizard.py
@@ -151,15 +151,32 @@ class SetupWizard:
     def generate_readme_txt(self):
         return f"""############################## Installation ##############################
 
-It is best to save the main directory in "C:-Users-(your user)
+This project REQUIRES python to be installed to the user's account. It
+also requires the user to have an active API key to access openai's models
+and to have the API key saved to a txt file. It is also recomended that 
+you install your desired png files for the assistant, user, and page icons
+and a jpg file for the background of the site (defaults to solid gray). Do
+note that the site icon will also be the desktop icon.
 
-Before proceeding further, open the api_key.txt file and paste you openAI
-API key and save the file.
+Start by running downloading and running "setup_wizard.py"
+This will open a setup UI that will guide you through the following steps
 
-Then run the install.bat file to install all application requirements and
-set up AI model. You will be able to set the desired model to use, the 
-temperature (creativity) of the model, and set a prompt to determine the 
-model's personality.
+Select desired install location:
+It is best to save the main directory in "C:-Users-(your user)" but it
+can be saved anywhere.
+
+Select your API key:
+Before proceeding further, you will have to select your API key file.
+THIS IS REQUIRED TO PROCEED ANY FURTHER
+
+Customize the theme:
+Here is where you will be able to change the UI header color, the 
+background image, and icons for the assistant, user, and webpage/desktop
+
+The script will then run the install.bat file to install all application 
+requirements and set up AI model. You will be able to set the desired 
+model to use, the temperature (creativity) of the model, and set a prompt 
+to determine the model's personality.
 
 When the installation is complete there will be a new shortcut on your
 desktop named "Custom AI" and at this point the setup is complete and the
@@ -677,7 +694,8 @@ pause"""
 Werkzeug==2.0.2
 openai==0.28
 PyPDF2
-pillow"""
+pillow
+python-docx"""
     def get_run_bat_content(self):
         return f"""@echo off
 
